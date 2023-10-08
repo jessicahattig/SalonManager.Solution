fdcefa2816807456b243af76000a493e8af5efd0
jessicahattig <jessicahattig@gmail.com>
2023-10-07 17:47:34 -0700

    Add Models.

---
+++ HairSalon/Models/Client.cs
@@ -0,0 +1,10 @@
+namespace ToDoList.Models
+{
+  public class Client
+  {
+    public int ClientId { get; set; }
+    public string Description { get; set; }
+    public int StylistId { get; set; }
+    public Stylist Stylist { get; set; }
+  }
+}
